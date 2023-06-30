1b553b93ff5ad158808380f9192e0659294ce6fa
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-06-30 19:07:28 -0400

    chore: :lipstick: Change Background and text color

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,12 @@
-import { component$ } from "@builder.io/qwik";
+import { component$ } from '@builder.io/qwik'
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
+} from '@builder.io/qwik-city'
+import { RouterHead } from './components/router-head/router-head'
 
-import "./global.css";
+import './global.css'
 
 export default component$(() => {
   /**
@@ -19,14 +19,14 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
+        <meta charSet='utf-8' />
+        <link rel='manifest' href='/manifest.json' />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body lang='en' class='h-screen bg-slate-800 text-emerald-100'>
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
     </QwikCityProvider>
-  );
-});
+  )
+})
