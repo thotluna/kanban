7dacde95e3b0d4a45683f3664edad9452b5328fb
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-07-06 21:45:04 -0400

    fix: :lipstick: Remove margin of Spring component

--- src/shared/components/spinner/index.tsx
+++ src/shared/components/spinner/index.tsx
@@ -5,7 +5,7 @@ export const Spinner = component$(() => {
     <div role='status'>
       <svg
         aria-hidden='true'
-        class='w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-emerald-600'
+        class='w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-emerald-600'
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
